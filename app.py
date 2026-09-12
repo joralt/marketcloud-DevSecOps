c48a6da372b71fc74aa9d11e09af7edd77c1a2ce
Joralt <jtravez18@gmail.com>
2026-09-12 11:13:54 -0500

    Arreglado

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ import subprocess
  
  
 def diagnostico(host):
-   comando = f"ping -c 1 {host}"
+    resultado = subprocess.run(
+        ["ping", "-c", "1", host],
+        capture_output=True,
+        text=True
+    )
  
-   resultado = subprocess.run(
-       comando,
-       shell=True,
-       capture_output=True,
-       text=True
-   )
- 
-   return resultado.stdout
+    return resultado.stdout
  
  
 host_usuario = input("Ingrese un host: ")
 print(diagnostico(host_usuario))
-
- 
